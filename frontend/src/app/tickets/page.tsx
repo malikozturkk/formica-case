7981bbed18564cebed23d0f086ceb4b535973371
malikozturkk <malikozturk975@gmail.com>
2025-03-21 12:13:42 +0300

    chore: change todo text

--- frontend/src/app/tickets/page.tsx
+++ frontend/src/app/tickets/page.tsx
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation'
 
 export default async function Tickets() {
-  //TODO: bana ait ticketları almak için accessToken ile apiye ssrda istek at
+  //TODO: bana ait ticketları al
   const cookieStore = await cookies()
   const accessToken = cookieStore.get("access_token")?.value;
   console.log(accessToken, "accessToken")
